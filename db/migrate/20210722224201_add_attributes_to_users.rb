4cc3a28abf3930178521259ed728b88b9b186fee
aliciapaz <aliciapazrojas@gmail.com>
2021-07-22 20:08:43 -0400

    Setup user model with roles

---
+++ db/migrate/20210722224201_add_attributes_to_users.rb
@@ -0,0 +1,9 @@
+class AddAttributesToUsers < ActiveRecord::Migration[6.1]
+  def change
+    change_table :users, bulk: true do |t|
+      t.column :name, :string, null: false
+      t.column :role, :integer, null: false
+      t.column :contact_info, :jsonb, null: false, default: {}
+    end
+  end
+end
