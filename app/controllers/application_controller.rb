aeb016a9e90cc931a9994a9c1683a1790d704eb8
aliciapaz <aliciapazrojas@gmail.com>
2021-07-23 21:59:20 -0400

    Validate fields in user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def index
+    render "home/index"
+  end
+
+  private
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role, { user_info: %i[phone address] }])
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   enum role: { admin: 0, buyer: 1, seller: 2 }
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 25 }
+  validate :phone_or_address
+
+  store_accessor :user_info, :phone, :address
+
+  private
+    def phone_or_address
+      if phone.blank? && address.blank?
+        errors.add(:user_info, "is required. You need to select an account type and provide your contact info")
+      end
+    end
 end
