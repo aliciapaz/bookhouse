d294dd4a3308412028697d9d190206b84a623618
aliciapaz <aliciapazrojas@gmail.com>
2021-07-24 15:34:32 -0400

    Validate role presence; and contact info only for buyers and sellers

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   enum role: { admin: 0, buyer: 1, seller: 2 }
 
   validates :name, presence: true, length: { minimum: 2, maximum: 25 }
+  validates :role, presence: true
   validate :phone_or_address
 
   store_accessor :user_info, :phone, :address, :balance
@@ -13,15 +14,14 @@ class User < ApplicationRecord
 
   private
     def phone_or_address
-      return unless phone.blank? && address.blank?
+      return unless phone.blank? && address.blank? || role == "admin"
 
       errors.add(:user_info, "is required. You need to select an account type and provide your contact info")
     end
 
     def update_initial_balance
-      return unless buyer?
-
-      user_info["balance"] = 50
+      user_info["balance"] = 50 if buyer?
+      user_info["balance"] = 0 if seller?
       save!
     end
 end
