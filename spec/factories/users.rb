d7ef6e5d50404a7a0f0fec7a5dd5b4f3de6b6b03
aliciapaz <aliciapazrojas@gmail.com>
2021-07-24 15:38:11 -0400

    Test user model validations and callbacks

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,24 @@
-# FactoryBot.define do
-#   factory :user do
-#   end
-# end
+FactoryBot.define do
+  factory :user, class: "User" do
+    name { Faker::Name.unique.name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    role { "admin" }
+  end
+
+  factory :seller, class: "User" do
+    name { Faker::Name.unique.name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    role { "seller" }
+    phone { Faker::PhoneNumber.phone_number }
+  end
+
+  factory :buyer, class: "User" do
+    name { Faker::Name.unique.name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    role { "buyer" }
+    address { Faker::Address.full_address }
+  end
+end
