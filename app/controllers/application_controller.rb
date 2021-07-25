b5647a33ca8d91ec96b9423b8dfd3c112f570e63
aliciapaz <aliciapazrojas@gmail.com>
2021-07-25 12:18:01 -0400

    Handle unauthorized action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   def index
     render "home/index"
   end
@@ -10,5 +12,10 @@ class ApplicationController < ActionController::Base
   private
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role, { user_info: %i[phone address balance] }])
+    end
+
+    def user_not_authorized
+      flash[:alert] = "You're not authorized for this action"
+      redirect_to root_path
     end
 end
