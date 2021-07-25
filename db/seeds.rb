a96ed955680ba1c010764cfbf32e4d79ea7c114b
aliciapaz <aliciapazrojas@gmail.com>
2021-07-25 12:36:23 -0400

    Seed users and books to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+Rails.logger.info "Seeding database"
+
+User.destroy_all
+3.times do
+  User.create!(name: Faker::Name.unique.name, email: Faker::Internet.email, password: Faker::Internet.password,
+               role: "seller", phone: Faker::PhoneNumber.phone_number)
+end
+
+Rails.logger.info "3 sellers created"
+
+Book.destroy_all
+18.times do
+  Book.create!(title: Faker::Book.title,
+               description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false),
+               author: Faker::Book.author,
+               price: "9.99",
+               seller_id: rand(1..3))
+end
+
+Rails.logger.info "18 books created"
