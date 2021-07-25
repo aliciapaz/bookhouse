d32c278386fa792ad45d2d17724574380c831d8a
aliciapaz <aliciapazrojas@gmail.com>
2021-07-24 22:37:13 -0400

    Validate book attributes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :seller, class_name: "User", inverse_of: :books
   has_one_attached :image
+
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :author, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :description, presence: true, length: { maximum: 2000 }
+  validates :price, presence: true, numericality: true
 end
