7270b5051b2164f3657d4b6689f18003c4730ab0
aliciapaz <aliciapazrojas@gmail.com>
2021-07-26 18:44:24 -0400

    Import jQuery library

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-require("bootstrap")
+import "bootstrap"
+import "jquery"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+window.jQuery = $;
+window.$ = $;
