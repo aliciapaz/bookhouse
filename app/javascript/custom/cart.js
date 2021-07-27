527a9db438ae556030e3d3e3707f324dad8e6398
aliciapaz <aliciapazrojas@gmail.com>
2021-07-27 12:18:21 -0400

    Fix cart count sentence in carts#show view

--- app/javascript/custom/cart.js
+++ app/javascript/custom/cart.js
@@ -24,8 +24,13 @@ $(window).on("turbolinks:load", () => {
     url = removeBtn.data('targeturl')
     $.ajax({url, type: 'put', success(data) {
       $('#cart-count').html(`My Cart: ${data}`);
-      $('span').html(data)
+      $('.cart-count-number').html(data)
+      $('.cart-count-noun').html(set_noun(data))
       $( e.target ).closest('.cart-book').slideUp()
     }})
   });
 })
+
+const set_noun = (data) => {
+  return (data == 1 ? " book" : " books")
+}
