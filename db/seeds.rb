73dead726aa1322a487eb6b7beacba89f65c26a5
aliciapaz <aliciapazrojas@gmail.com>
2021-07-27 09:47:46 -0400

    Seed an admin user to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,10 @@ require "faker"
 Rails.logger.info "Seeding database"
 
 User.destroy_all
+
+User.create!(name: "admin", email: "admin@bookhouse.com", password: "bookhouse",
+             role: "admin")
+
 3.times do
   User.create!(name: Faker::Name.unique.name, email: Faker::Internet.email, password: Faker::Internet.password,
                role: "seller", phone: Faker::PhoneNumber.phone_number)
@@ -10,13 +14,18 @@ end
 
 Rails.logger.info "3 sellers created"
 
+# Store ids of the sellers just created
+first = User.first.id + 1
+last = User.last.id
+
 Book.destroy_all
+
 18.times do
   Book.create!(title: Faker::Book.title,
                description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false),
                author: Faker::Book.author,
                price: "9.99",
-               seller_id: rand(1..3))
+               seller_id: rand(first..last))
 end
 
 Rails.logger.info "18 books created"
