457477cb29b672c7a704b4250ad79b61f12633ab
aliciapaz <aliciapazrojas@gmail.com>
2021-07-27 11:19:30 -0400

    Add status to books

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,10 @@ class Book < ApplicationRecord
   belongs_to :seller, class_name: "User", inverse_of: :books
   has_one_attached :image
 
+  enum status: { available: 0, taken: 1, sold: 2 }
+
+  scope :available, -> { where(status: "available") }
+
   validates :title, presence: true, length: { maximum: 100 }
   validates :author, presence: true, length: { minimum: 2, maximum: 50 }
   validates :description, presence: true, length: { maximum: 2000 }
