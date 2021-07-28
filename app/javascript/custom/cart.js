3ff6b06e818208bf694c05338274e5772ce86630
aliciapaz <aliciapazrojas@gmail.com>
2021-07-27 22:07:42 -0400

    Fix bug on removing books

--- app/javascript/custom/cart.js
+++ app/javascript/custom/cart.js
@@ -1,4 +1,4 @@
-// Add to and remove from cart on book show page
+// Add to and remove from cart on book's show page
 $(window).on("turbolinks:load", () => {
   $('#cart-action-btn').on('click', (e) => {
     e.preventDefault();
@@ -17,28 +17,19 @@ $(window).on("turbolinks:load", () => {
       cartActionBtn.data('target', new_target)
     }})
   })
-// Remove from cart on cart show page
+// Remove book from cart on cart show page
   $('#my-cart .remove').on('click', (e) => {
     e.preventDefault();
     let removeBtn, url;
-    removeBtn = $('#my-cart .remove').closest('a')
-    url = removeBtn.data('targeturl')
+    $this = $(e.target)
+    url = $this.data('targeturl')
     $.ajax({url, type: 'put', success(data) {
       $('#cart-count').html(`My Cart: ${data}`);
       $('.cart-count-number').html(data)
       $('.cart-count-noun').html(set_noun(data))
-      $( e.target ).closest('.cart-book').slideUp()
+      $this.closest('.cart-book').slideUp()
     }})
   });
-// Confirm purchase and update content
-  $('.confirm').on('click', (e) => {
-    e.preventDefault();
-    let url = $('.confirm').data('targeturl')
-    $.ajax({url, type: 'put', datatype: 'json', success(data) {
-      $('#user-balance').html(`My Balance: $${data.balance}`)
-      $('#cart-count').html(`My Cart: ${data.cart}`)
-    }})
-  })
 })
 
 const set_noun = (data) => {
---
+++ app/javascript/custom/purchase.js
@@ -0,0 +1,15 @@
+// Confirm purchase and update content
+$(window).on('turbolinks:load', () => {
+  
+  $('.confirm').on('click', (e) => {
+    e.preventDefault();
+    let url = $('.confirm').data('targeturl')
+    $.ajax({url, type: 'put', datatype: 'json', success(data) {
+      $('#user-balance').html(`My Balance: $${data.balance}`)
+      $('#cart-count').html(`My Cart: ${data.cart}`)
+    }})
+  })
+
+  $('')
+
+})
